8e3eec0865a029061bcc74c8fa87c24b6136ccec
alivemp <lucascjosaphat@gmail.com>
2024-06-06 16:09:43 -0700

    The final results of the weakness project

--- pokemon.py
+++ pokemon.py
@@ -1,49 +1,43 @@
-from bs4 import BeautifulSoup
+import json
 import requests
-
-
-url = "https://pokemondb.net/pokedex/all"
-
-page = requests.get(url)
-
-soup = BeautifulSoup(page.text, "html")
 
 # Assign typing weaknesses
 typings = {
-    "Normal":   {"weak": ["Fighting"], "resist": [], "immune": ["Ghost"]},
-    "Fire":     {"weak": ["Water", "Ground", "Rock"], "resist": ["Fire", "Grass", "Ice", "Bug", "Steel", "Fairy"], "immune": []},
-    "Water":    {"weak": ["Grass", "Electric"], "resist": ["Fire", "Water", "Ice", "Steel"], "immune": []},
-    "Electric": {"weak": ["Ground"], "resist": ["Electric", "Flying", "Steel"], "immune": []},
-    "Grass":    {"weak": ["Fire", "Ice", "Poison", "Flying", "Bug"], "resist": ["Water", "Electric", "Grass", "Ground"], "immune": []},
-    "Ice":      {"weak": ["Fire", "Fighting", "Rock", "Steel"], "resist": ["Ice"], "immune":[]},
-    "Fighting": {"weak": ["Flying", "Psychic", "Fairy"], "resist": ["Bug", "Rock", "Dark"], "immune": []},
-    "Poison":   {"weak": ["Ground", "Psychic"], "resist": ["Grass", "Fighting", "Poison", "Bug", "Fairy"], "immune":[]},
-    "Ground":   {"weak": ["Water", "Grass", "Ice"], "resist": ["Posion", "Rock"], "immune": ["Electric"]},
-    "Flying":   {"weak": ["Electric", "Ice", "Rock"], "resist": ["Grass", "Fighting", "Bug"], "immune": ["Ground"]},
-    "Psychic":  {"weak": ["Bug", "Ghost", "Dark"], "resist": ["Fighting", "Psychic"], "immune": []},
-    "Bug":      {"weak": ["Fire", "Flying", "Rock"], "resist": ["Grass", "Fighting", "Ground"], "immune": []},
-    "Rock":     {"weak": ["Water", "Grass", "Fighting", "Ground", "Steel"], "resist": ["Normal", "Fire", "Poison", "Flying"], "immune": []},
-    "Ghost":    {"weak": ["Ghost", "Dark"], "resist": ["Poison", "Bug"], "immune": ["Normal", "Fighting"]},
-    "Dragon":   {"weak": ["Ice", "Dragon", "Fairy"], "resist": ["Fire", "Water", "Electric", "Grass"], "immune": []},
-    "Dark":     {"weak": ["Fighting", "Bug", "Fairy"], "resist": ["Ghost", "Dark",], "immune": ["Psychic"]},
-    "Steel":    {"weak": ["Fire", "Fighting", "Ground"], "resist": ["Normal", "Grass", "Ice","Flying", "Psychic", "Bug", "Rock", "Dragon", "Steel", "Fairy"], "immune": ["Poison"]},
-    "Fairy":    {"weak": ["Poison", "Steel"], "resist": ["Fighting", "Bug", "Dark"], "immune": ["Dragon"]},
+    "normal":   {"weak": ["fighting"], "resist": [], "immune": ["ghost"]},
+    "fire":     {"weak": ["water", "ground", "rock"], "resist": ["fire", "grass", "ice", "bug", "steel", "fairy"], "immune": []},
+    "water":    {"weak": ["grass", "electric"], "resist": ["fire", "water", "ice", "steel"], "immune": []},
+    "electric": {"weak": ["ground"], "resist": ["electric", "flying", "steel"], "immune": []},
+    "grass":    {"weak": ["fire", "ice", "poison", "flying", "bug"], "resist": ["water", "electric", "grass", "ground"], "immune": []},
+    "ice":      {"weak": ["fire", "fighting", "rock", "steel"], "resist": ["ice"], "immune":[]},
+    "fighting": {"weak": ["flying", "psychic", "fairy"], "resist": ["bug", "rock", "dark"], "immune": []},
+    "poison":   {"weak": ["ground", "psychic"], "resist": ["grass", "fighting", "poison", "bug", "fairy"], "immune":[]},
+    "ground":   {"weak": ["water", "grass", "ice"], "resist": ["poison", "rock"], "immune": ["electric"]},
+    "flying":   {"weak": ["electric", "ice", "rock"], "resist": ["grass", "fighting", "bug"], "immune": ["ground"]},
+    "psychic":  {"weak": ["bug", "ghost", "dark"], "resist": ["fighting", "psychic"], "immune": []},
+    "bug":      {"weak": ["fire", "flying", "rock"], "resist": ["grass", "fighting", "ground"], "immune": []},
+    "rock":     {"weak": ["water", "grass", "fighting", "ground", "steel"], "resist": ["normal", "fire", "poison", "flying"], "immune": []},
+    "ghost":    {"weak": ["ghost", "dark"], "resist": ["poison", "bug"], "immune": ["normal", "fighting"]},
+    "dragon":   {"weak": ["ice", "dragon", "fairy"], "resist": ["fire", "water", "electric", "grass"], "immune": []},
+    "dark":     {"weak": ["fighting", "bug", "fairy"], "resist": ["ghost", "dark",], "immune": ["psychic"]},
+    "steel":    {"weak": ["fire", "fighting", "ground"], "resist": ["normal", "grass", "ice", "flying", "psychic", "bug", "rock", "dragon", "steel", "fairy"], "immune": ["poison"]},
+    "fairy":    {"weak": ["poison", "steel"], "resist": ["fighting", "bug", "dark"], "immune": ["dragon"]},
 }
 # Set up a way for the pokemon to be stored
 
-class pokedex:
-    def __init__(self, dexnum, name, types, stats):
-        self.dexnum = dexnum
-        self.name = name
+class Pokedex:
+    def __init__(self,types,):
         self.types = types
-        self.stats = stats
+
 # Figure out how to make weaknesses
     def weakness_finder(self):
+        types_list = self.types.split(", ")
+        
         weaknesses = set()
         resistances = set()
         immunites = set()
-
-        for pokemon in self.types:
+        
+        
+        for pokemon in types_list:
             if pokemon in typings:
                 weaknesses.update(typings[pokemon]["weak"])
                 resistances.update(typings[pokemon]["resist"])
@@ -53,18 +47,38 @@ class pokedex:
         weaknesses -= immunites
 
 
-# Figure out how to put pokemon in the pokedex class established earlier
+        return list(weaknesses)
+    
+    def show_weakness(self) :
+        if isinstance(self.types, str) :
+            type_str = self.types
+        else:
+            type_str = ", ".join(self.types)
+            
+        weaknesses = self.weakness_finder()
+        weaknesses_str = ", ".join(weaknesses)
+        print(f"The pokemon's type is {type_str.title()}")
+        print(f"The weaknesses are {weaknesses_str.title()}")
 
-soup.find_all("a")
 
-pokemans = []
 
-pokemans = soup.text
-
-for pokemans in pokedex:
-    s
 
 #Get user input to search with later
-search = input("What pokemon do you want to look for ")
+while True :
+    search = input("What pokemon do you want to look for ")
 
-print(pokemans)
+    # Figure out how to put pokemon in the pokedex class established earlier
+
+    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{search}")
+
+    poketype = response.json()["types"]
+
+    poketype2 = [type_data["type"]["name"] for type_data in poketype]
+
+    poketype3 = ", ".join([str(item) for item in poketype2])
+
+    pokedex_instance = Pokedex(poketype3)
+    pokedex_instance.show_weakness()
+    end = input("Type end to end the program ")
+    if end == "end":
+        break
